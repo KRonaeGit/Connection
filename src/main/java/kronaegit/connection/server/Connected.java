3c7a186a1c79e90eee9bc0251537e16eee809f7c
KRonaeGit <161943142+KRonaeGit@users.noreply.github.com>
2024-11-10 15:01:01 +0900

    Delete src directory

--- src/main/java/kronaegit/connection/server/Connected.java
+++
@@ -1,9 +0,0 @@
-package kronaegit.connection.server;
-
-import kronaegit.connection.client.Connector;
-
-import java.io.IOException;
-
-public interface Connected {
-    void onConnect(Connector connector) throws IOException;
-}
